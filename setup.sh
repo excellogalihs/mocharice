273968245e075c47cb1b5ba0ff09e3e2771bcf7c
excellogalihs <excellogalihs@gmail.com>
2026-08-17 13:34:53 +0700

    A few tweaks

--- setup.sh
+++ setup.sh
@@ -11,5 +11,5 @@ cp -rf ~/mocharice/powerlevel10k ~/
 cp -rf ~/mocharice/.p10k.zsh ~/
 cp -rf ~/mocharice/.zshrc ~/
 cp -rf ~/mocharice/.config/{fastfetch,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
-awww img ~/mocharice/wallpapers/storm.jpg
+awww img ~/mocharice/wallpapers/piano.jpg
 reboot
