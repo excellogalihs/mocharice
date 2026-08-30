5128cc46294880c39efbc4f261ad8941a85c43ee
excellogalihs <excellogalihs@gmail.com>
2026-08-30 08:05:26 +0700

    Some gtk things

--- setup.sh
+++ setup.sh
@@ -21,11 +21,10 @@ Include = /etc/pacman.d/mirrorlist
 Include = /etc/pacman.d/mirrorlist
 EOF
 chsh -s $(which zsh)
-cp -rf ~/mocharice/.gtkrc-2.0 ~/
 cp -rf ~/mocharice/.tmux.conf ~/
 cp -rf ~/mocharice/.zshrc ~/
 cp -rf ~/mocharice/.config/omp.toml ~/.config/
-cp -rf ~/mocharice/.config/{fastfetch,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
+cp -rf ~/mocharice/.config/{fastfetch,gtk-3.0,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
 tmux new -ds space
 awww img ~/mocharice/wallpapers/plane.jpg
 reboot
