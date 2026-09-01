dd62a59c46d8a5cae8360544a541cbf4c4e71d4a
excellogalihs <excellogalihs@gmail.com>
2026-09-01 20:53:11 +0700

    New neovim plugin

---
+++ .config/nvim/lua/plugins/live-preview.lua
@@ -0,0 +1,5 @@
+return {
+  "brianhuster/live-preview.nvim",
+  opts = { browser_server_refresh = false },
+  keys = { { "<leader>h", "<cmd>w<CR><cmd>LivePreview start<CR>", desc = "HTML Live Preview" } },
+}
