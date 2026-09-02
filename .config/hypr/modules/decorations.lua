28f2e22b1eb393c911f76937c1143dd9c3ac6c07
excellogalihs <excellogalihs@gmail.com>
2026-09-02 20:23:05 +0700

    Tweak animations

--- .config/hypr/modules/decorations.lua
+++ .config/hypr/modules/decorations.lua
@@ -1,33 +1,33 @@
 hl.config({
-  general = {
-    gaps_in = 5,
-    gaps_out = 15,
-    border_size = 2,
-    col = {
-      active_border = "#cba6f7",
-      inactive_border = "#1e1e2e",
-    },
-    layout = "dwindle",
-  },
-  decoration = {
-    active_opacity = 1.0,
-    inactive_opacity = 0.5,
-    blur = {
-      enabled = true,
-      size = 3,
-      passes = 3,
-    },
-  },
-  animations = {
-    enabled = true,
-  },
+	general = {
+		gaps_in = 5,
+		gaps_out = 15,
+		border_size = 2,
+		col = {
+			active_border = "#cba6f7",
+			inactive_border = "#1e1e2e",
+		},
+		layout = "dwindle",
+	},
+	decoration = {
+		active_opacity = 1.0,
+		inactive_opacity = 0.5,
+		blur = {
+			enabled = true,
+			size = 3,
+			passes = 3,
+		},
+	},
+	animations = {
+		enabled = true,
+	},
 })
 hl.curve("smooth", {
-  type = "bezier",
-  points = {
-    { 0.1, 1.2 },
-    { 0.2, 1.0 }
-  }
+	type = "bezier",
+	points = {
+		{ 0.1, 1.1 },
+		{ 0.2, 1.0 },
+	},
 })
 hl.config({ animations = { enabled = true } })
 hl.animation({ leaf = "border", enabled = true, speed = 5, bezier = "smooth" })
