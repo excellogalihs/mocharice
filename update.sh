3c7ccaf611479c5108e274d498334dc3b4f28b26
excellogalihs <excellogalihs@gmail.com>
2026-09-22 18:55:39 +0700

    Tweaks

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo pacman -S --noconfirm awww base-devel bat breeze-gtk eza fastfetch fd fzf git grim gtk3 hyprland hyprlock hyprpolkitagent kitty nvim papirus-icon-theme power-profiles-daemon ripgrep tmux tree-sitter-cli ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard wofi xdg-desktop-portal-hyprland yazi zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
-yay -S --noconfirm polycat wlogout oh-my-posh-bin zen-browser-bin spotify-adblock-git
-sudo pacman -Syu
-yay -Syu
 cp -rf ~/mocharice/.tmux.conf ~/
 cp -rf ~/mocharice/.zshrc ~/
 cp -rf ~/mocharice/.config/omp.toml ~/.config/
 cp -rf ~/mocharice/.config/{fastfetch,gtk-3.0,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
+sudo pacman -S --noconfirm awww base-devel bat breeze-gtk eza fastfetch fd fzf git grim gtk3 hyprland hyprlock hyprpolkitagent kitty nvim papirus-icon-theme power-profiles-daemon ripgrep tmux tree-sitter-cli ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard wofi xdg-desktop-portal-hyprland yazi zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
+yay -S --noconfirm polycat wlogout oh-my-posh-bin zen-browser-bin spotify-adblock-git
+sudo pacman -Syu
+yay -Syu
 command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'
