562626392c3f7f30a600f21838793324cdb0a7e2
excellogalihs <excellogalihs@gmail.com>
2026-09-26 11:26:34 +0700

    Bug fixes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
-sudo pacman -S --noconfirm awww base-devel bat breeze-gtk eza fastfetch fd fzf git grim gtk3 hyprland hyprlock hyprpolkitagent kitty nvim papirus-icon-theme power-profiles-daemon ripgrep tmux tree-sitter-cli ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard wofi xdg-desktop-portal-hyprland yazi zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S --noconfirm awww base-devel bat breeze-gtk eza fastfetch fd fzf git grim gtk3 gtk4 hyprland hyprlock hyprpolkitagent kitty nvim papirus-icon-theme power-profiles-daemon ripgrep tmux tree-sitter-cli ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard wofi xdg-desktop-portal-hyprland yazi zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
 rm -rf yay
 git clone https://aur.archlinux.org/yay
 cd yay
@@ -21,10 +20,9 @@ Include = /etc/pacman.d/mirrorlist
 [extra]
 Include = /etc/pacman.d/mirrorlist
 EOF
-chsh -s $(which zsh)
+chsh -s /usr/bin/zsh
 cp -rf ~/mocharice/.tmux.conf ~/
 cp -rf ~/mocharice/.zshrc ~/
 cp -rf ~/mocharice/.config/omp.toml ~/.config/
-cp -rf ~/mocharice/.config/{fastfetch,gtk-3.0,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
-awww img ~/mocharice/wallpapers/plane.jpg
+cp -rf ~/mocharice/.config/{fastfetch,gtk-3.0,gtk-4.0,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
 reboot
