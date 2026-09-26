f61b975294074e763f7fa5ba2061327245123c24
excellogalihs <excellogalihs@gmail.com>
2026-09-26 11:28:34 +0700

    Bug fixes

--- updates.sh
+++ updates.sh
@@ -11,5 +11,5 @@ cp -rf ~/mocharice/.config/omp.toml ~/.config/
 cp -rf ~/mocharice/.config/{fastfetch,gtk-3.0,gtk-4.0,hypr,kitty,nvim,waybar,wlogout,wofi} ~/.config/
 sudo pacman -S --noconfirm awww base-devel bat breeze-gtk eza fastfetch fd fzf git grim gtk3 gtk4 hyprland hyprlock hyprpolkitagent kitty nvim papirus-icon-theme power-profiles-daemon ripgrep tmux tree-sitter-cli ttf-jetbrains-mono-nerd waybar wiremix wl-clipboard wofi xdg-desktop-portal-hyprland yazi zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
 yay -S --noconfirm polycat wlogout oh-my-posh-bin zen-browser-bin
-sudo pacman -Syu
-yay -Syu
+sudo pacman -Syu --noconfirm
+yay -Syu --noconfirm
